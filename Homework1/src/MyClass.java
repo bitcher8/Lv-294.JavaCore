beccc8a8cccd1010508793d66c93610efeb3ad55
MaxymSydoriv <maxym19998@gmail.com>
2018-01-30 23:49:17 +0200

    My first homework

---
+++ Homework1/src/MyClass.java
@@ -0,0 +1,48 @@
+import java.io.*;
+public class MyClass {
+
+	public static void main(String[] args) throws IOException{
+		System.out.println("First Task");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the radius");
+		double radius = Double.parseDouble(reader.readLine());
+		System.out.println("Perimeter:" + (2*radius*Math.PI));
+		System.out.println("Area:" + (Math.PI *Math.pow(radius, 2)));
+		System.out.println();
+		
+		
+		System.out.println("Second Task");
+		System.out.println("What's your name?");
+		String name = reader.readLine();
+		System.out.println("Where do you live, " + name + "?");
+		String address = reader.readLine();
+		System.out.println(name + "\n" + address);
+		
+		
+		System.out.println("Third Task");
+		
+		System.out.println("Enter price of phonecall from the first country");
+        double c1 = Double.parseDouble(reader.readLine());
+        System.out.println("Enter the time how much the first phonecall takes");
+	    double t1 = Double.parseDouble(reader.readLine());
+	    
+	    System.out.println("Enter price of phonecall from the second country");
+        double c2 = Double.parseDouble(reader.readLine());
+        System.out.println("Enter the time how much the second phonecall takes");
+	    double t2 = Double.parseDouble(reader.readLine());
+	    
+	    System.out.println("Enter price of phonecall from the third country");
+        double c3 = Double.parseDouble(reader.readLine());
+        System.out.println("Enter the time how much the third phonecall takes");
+	    double t3 = Double.parseDouble(reader.readLine());
+	    
+	    double ct1 = c1*t1;
+	    double ct2 = c2*t2;
+	    double ct3 = c3*t3;
+	    System.out.println("Computer will count for first call " + ct1);
+	    System.out.println("Computer will count for first call " + ct2);
+	    System.out.println("Computer will count for first call " + ct3);
+	    System.out.println("Together: "+ (ct1+ct2+ct3));
+	}
+
+}
