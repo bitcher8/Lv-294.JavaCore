8746a0eee49d45d68abe4e738fdaad95a5454b9e
Administrator <Administrator@WIN-7V945D2C>
2018-01-30 23:58:42 +0200

    new class

--- HW1/src/Circle.java
+++ HW1/src/Circle.java
@@ -1,10 +1,16 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Circle {
+	public static void meth() throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(
+	new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+System.out.println("How are you?");
+String answer = br.readLine();
+System.out.println("You are" + answer);  
 
-	}
+}	
 
 }
