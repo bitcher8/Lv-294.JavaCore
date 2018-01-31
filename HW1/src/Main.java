5c806f15baf2893dffa20732ba298e54d8698216
Administrator <Administrator@WIN-7V945D2C>
2018-02-01 00:50:15 +0200

    3rd task

--- HW1/src/Main.java
+++ HW1/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		//Ex1Circle.perimeter();
-		Ex2Person.io();
-
+		//Ex2Person.io();
+		Ex3Calls.billing();
 	}
 
 }
